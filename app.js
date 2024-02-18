42a7ff3ace9bfb8929deb36ce1d31c3cbbb45209
Naitchi <assasindu39@gmail.com>
2024-02-19 00:07:12 +0100

    ✨ create first chat

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-const index = require('./routes/index');
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
 
-app.use('/api/', index);
+io.on('connection', (socket) => {
+  console.log('Un client est connecté');
 
-module.exports = app;
+  socket.on('message', (data) => {
+    console.log('Message reçu : ', data);
+    io.emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Un client est déconnecté');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+});
