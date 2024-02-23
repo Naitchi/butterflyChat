e7c85e6f1a977aa0bd5eec778cf566d4f1a7281d
Naitchi <assasindu39@gmail.com>
2024-02-23 11:28:07 +0100

    🐛 Added Rooms

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const pusher = require('../pusher');
 async function message(req, res) {
   console.log('message');
   const payload = req.body;
-  await pusher.trigger('butterflychat', 'message', payload.message);
+  await pusher.trigger(`butterflychat-${payload.idRoom}`, 'message', payload.message);
   res.status(200).send(payload);
 }
 
